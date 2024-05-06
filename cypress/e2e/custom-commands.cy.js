36dfb84c495169b5bc578af9388a989660673695
unknown <lukmanolayiwola125@gmail.com>
2024-05-06 21:32:50 +0100

    This is User Registration

---
+++ cypress/e2e/custom-commands.cy.js
@@ -0,0 +1,17 @@
+describe('Custom Command', ()=>{
+
+it('Testing Custom Commands', ()=>{
+    cy.visit('https://parabank.parasoft.com/parabank/index.htm')
+    cy.CustomCommand("username")
+
+
+
+})
+
+
+
+
+
+
+
+})
